9542847509d3d4730a7a8f0607e1b2bf24026a78
Eliezer Abate <eliezerabate@gmail.com>
2019-02-25 16:05:19 -0800

    [user_profile] - added successful user profile template on login

--- login.php
+++ login.php
@@ -1,4 +1,47 @@
 <?php
+function getProfilePage($firstname) {
+return <<<HTML
+  <html>
+    <head>
+      <title>Cakebook</title>
+    </head>
+    <body>
+     <section>
+       <form action="search.php" method="post">
+        <label for="search">Search:</label>
+        <input type="textarea" name="search" id="search">
+        <input type="submit" value="search">
+       </form>
+       <ul>
+         <li>{$firstname}</li>
+         <li>Account</li>
+         <li>Settings</li>
+       </ul>
+     </section>
+     <main>
+      <nav>
+        <ul>
+          <li>Friends</li>
+          <li>Pages</li>
+          <li>Events</li>
+          <li>Groups</li>
+          <li>Saved</li>
+        </ul>
+      </nav>
+      <section>
+        <form action="post.php" action="post">
+          <label for="post">Create Post:</label> 
+          <input type="textarea" name="post">
+          <input type="submit" name="Post">
+        </form>
+        <main id="feed">NewsFeed</main>
+      </section>
+     </main>
+    </body>
+  </html>
+HTML;
+}
+
 function authenticateUser($username, $password) {
   $conn = new mysqli("localhost", "eliezerabate", "password", "CakebookDB");
   if ($conn->connect_error) {
@@ -8,7 +51,7 @@ function authenticateUser($username, $password) {
   $result = $conn->query($sql);
   if ($result->num_rows > 0) {
     while($row = $result->fetch_assoc()) {
-      echo "Welcome " . $row["firstName"] . "\n"; 
+      echo getProfilePage($row["firstName"]); 
     }
   } else {
     echo $sql;
@@ -16,5 +59,7 @@ function authenticateUser($username, $password) {
   $conn->close();
 }
 
+
 authenticateUser($_POST["username"],$_POST["password"]);
+
 ?>
---
+++ user_profile.php
@@ -0,0 +1,32 @@
+<html>
+  <head>
+    <title>Cakebook</title>
+  </head>
+  <body>
+   <section>
+    <label for="search">Search:</label>
+    <input type="textarea" name="search" id="search">
+    <ul>
+      <li>Dynamic Name</li>
+      <li>Account</li>
+      <li>Settings</li>
+    </ul>
+   </section>
+   <main>
+    <nav>
+      <ul>
+        <li>Friends</li>
+        <li>Pages</li>
+        <li>Events</li>
+        <li>Groups</li>
+        <li>Saved</li>
+      </ul>
+    </nav>
+    <section>
+      <label for="post">Create Post:</label> 
+      <input type="textarea" name="post">
+      <main id="feed">NewsFeed</main>
+    </section>
+   </main>
+  </body>
+</html>
